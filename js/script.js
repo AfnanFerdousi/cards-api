aa3d8730527d67d07de9a52b81658444188cc11d
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-02-23 23:49:48 +0600

    done

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const searchButton = () => {
     }
     else {
         main.innerHTML = "";
-        
+
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
             .then(res => res.json())
             .then(data => cardsDisplay(data.cards))
@@ -38,10 +38,32 @@ const cardsDisplay = (cards) => {
                 <div class="card-body">
                     <h5 class="card-title">${card.suit}</h5>
                     <p class="card-text">${card.code}</p>
-                    <a href="#" class="btn btn-primary">Details</a>
+                    <button onclick="cardDetails('${card.code}')" href="#" class="btn btn-primary">Details</button>
                 </div>
             </div>                    
         `
         main.appendChild(div);
     }    
+}
+
+const cardDetails = (code) => {
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+            .then(res => res.json())
+        .then(data => {
+            const allCards = data.cards;
+            const singleCard = allCards.find(card => card.code === code)
+            const div = document.createElement('div');
+            main.innerHTML ="";
+             div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img src="${singleCard.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${singleCard.suit}</h5>
+                        <p class="card-text">${singleCard.code}</p>
+                        <p class="card-text">${singleCard.value}</p>                      
+                    </div>
+                </div>                    
+        `
+            main.appendChild(div);
+        })
 }
